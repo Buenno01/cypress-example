8303b2c7ef91b512160bac1b76484c3338e803f3
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-05-20 21:49:22 -0300

    refactor: change RemoteAuthentication to use faker

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,13 @@
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemotheAuthentication } from './remote-authentication'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RemotheAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url: string = 'any_url'): SutTypes => { // factory
+const makeSut = (url: string = faker.internet.url()): SutTypes => { // factory
   const httpPostClientSpy = new HttpPostClientSpy()
   const sut = new RemotheAuthentication(url, httpPostClientSpy)
   return {
@@ -17,7 +18,7 @@ const makeSut = (url: string = 'any_url'): SutTypes => { // factory
 
 describe('Remote Authentication', () => {
   test('Should call HttpClient with correct URL', async () => {
-    const url = 'other_url'
+    const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
