1ccc8b4967356b10ee18bad5882d233f2b20185b
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-06-13 16:00:00 -0300

    refactor: move logo to a separate file

---
+++ src/presentation/components/loader/loader.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Styles from './loader-styles.scss'
+
+type Props = React.HTMLAttributes<HTMLElement>
+
+const Loader: React.FC<Props> = (props: Props) => {
+  return (<div className={[Styles.loader, props.className].join(' ')}><div /><div /><div /><div /><div /><div /><div /><div /></div>)
+}
+
+export default Loader
