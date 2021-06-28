73cb493553389fa47c51cee8bbe22674f49ec5a0
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-06-28 15:58:54 -0300

    feat: ensure Login calls Validation with correct password

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -11,7 +11,8 @@ type Props = {
 const Login: React.FC<Props> = ({ validation }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
-    email: ''
+    email: '',
+    password: ''
   })
   const [errorState] = useState({
     email: 'Campo obrigatório',
@@ -22,6 +23,10 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   useEffect(() => {
     validation.validate({ email: state.email })
   }, [state.email])
+
+  useEffect(() => {
+    validation.validate({ password: state.password })
+  }, [state.password])
 
   return (
     <div className={Styles.login}>
