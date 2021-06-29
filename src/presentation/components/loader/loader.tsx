7c91c67c999ee11f652b78a97388469d0e4c60c3
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-06-29 16:09:41 -0300

    feat: ensure Login present loader on submit

--- src/presentation/components/loader/loader.tsx
+++ src/presentation/components/loader/loader.tsx
@@ -4,7 +4,7 @@ import Styles from './loader-styles.scss'
 type Props = React.HTMLAttributes<HTMLElement>
 
 const Loader: React.FC<Props> = (props: Props) => {
-  return (<div className={[Styles.loader, props.className].join(' ')}><div /><div /><div /><div /><div /><div /><div /><div /></div>)
+  return (<div data-testid="loader" className={[Styles.loader, props.className].join(' ')}><div /><div /><div /><div /><div /><div /><div /><div /></div>)
 }
 
 export default Loader
