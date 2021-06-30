994b56fe8483fd7d58fc75b58a323f98b00cac4a
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-06-30 15:34:06 -0300

    test: ensure EmailValidation returns falsy if email is valid

---
+++ src/validation/validators/email/email-validation.spec.ts
@@ -0,0 +1,17 @@
+import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
+import { EmailValidation } from './email-validation'
+import faker from 'faker'
+
+describe('EmailValidation', () => {
+  test('Should return error if email is invalid', () => {
+    const sut = new EmailValidation(faker.database.column())
+    const error = sut.validate(faker.random.word())
+    expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return false if email is valid', () => {
+    const sut = new EmailValidation(faker.database.column())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
+})
