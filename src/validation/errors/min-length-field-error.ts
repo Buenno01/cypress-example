67e779af25e5c29b2f55b5def5307d724b064bfd
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-06-30 15:42:50 -0300

    feat: ensure MinLengthValidation returns error if value is invalid

---
+++ src/validation/errors/min-length-field-error.ts
@@ -0,0 +1,5 @@
+export class MinLengthFieldError extends Error {
+  constructor () {
+    super('Tamanho mínimo de caracteres inválido')
+  }
+}
---
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -0,0 +1,9 @@
+import { MinLengthFieldError } from '@/validation/errors/min-length-field-error'
+import { FieldValidation } from '@/validation/protocols'
+
+export class MinLengthValidation implements FieldValidation {
+  constructor (readonly name, private readonly minLength: number) {}
+  validate (value: string): Error {
+    return new MinLengthFieldError()
+  }
+}
