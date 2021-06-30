e6d30d32ee338eb6a859ceeb544fda7bf148524b
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-06-30 15:43:15 -0300

    test: ensure MinLengthValidation returns error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,10 @@
+import { MinLengthFieldError } from '@/validation/errors/min-length-field-error'
+import { MinLengthValidation } from './min-length-validation'
+
+describe('MinLengthValidation', () => {
+  test('Should return error if value is invalid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('123')
+    expect(error).toEqual(new MinLengthFieldError())
+  })
+})
