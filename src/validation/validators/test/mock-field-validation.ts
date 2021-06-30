851c66b9086937db747eaf65f76ece114c291a85
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-06-30 16:47:19 -0300

    feat: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/test/mock-field-validation.ts
@@ -0,0 +1,9 @@
+import { FieldValidation } from '@/validation/protocols'
+
+export class FieldValidationSpy implements FieldValidation {
+  error: Error = null
+  constructor (readonly name: string) {}
+  validate (value: string): Error {
+    return this.error
+  }
+}
