266314b2af2df4039ff0fb7354cd2c0e03bd2f85
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-07-11 02:08:03 -0300

    test: ensure RemoteAddAccount call HttpClient with correct URL

---
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -0,0 +1,28 @@
+import { HttpPostClientSpy } from '@/data/test'
+import { RemoteAddAccount } from './remote-add-account'
+import faker from 'faker/locale/en_BORK'
+import { AddAccount } from '@/domain/usecases'
+import { mockAddAccountParams } from '@/domain/test'
+
+type SutTypes = {
+  sut: RemoteAddAccount
+  httpPostClientSpy: HttpPostClientSpy<AddAccount.Params, AddAccount.Model>
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy<AddAccount.Params, AddAccount.Model>()
+  const sut = new RemoteAddAccount(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
+describe('RemoteAddAccount', () => {
+  test('Should call HttpClient with correct URL', async () => {
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.add(mockAddAccountParams())
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
---
+++ src/domain/test/mock-add-account.ts
@@ -0,0 +1,16 @@
+import { AddAccount } from '@/domain/usecases/'
+import faker from 'faker'
+
+export const mockAddAccountParams = (): AddAccount.Params => {
+  const password = faker.internet.password()
+  return {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: password,
+    passwordConfirmation: password
+  }
+}
+
+export const mockAddAccountModel = (): AddAccount.Model => ({
+  accessToken: 'mock-access-token'
+})
