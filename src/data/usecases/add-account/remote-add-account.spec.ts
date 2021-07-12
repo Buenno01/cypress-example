4d7b2362158b81e3815ef847a387238521d5c7ff
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-07-12 13:25:54 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 404

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -55,4 +55,11 @@ describe('RemoteAddAccount', () => {
     const promise = sut.add(mockAddAccountParams())
     await expect(promise).rejects.toThrowError(new UnexpectedError())
   })
+
+  test('Should throw UnexpectedError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response.statusCode = HttpStatusCode.notFound
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrowError(new UnexpectedError())
+  })
 })
