3e11420759716845f0765f8bdb0fc01a8d9a363a
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-07-12 13:25:02 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 500

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -48,4 +48,11 @@ describe('RemoteAddAccount', () => {
     const promise = sut.add(mockAddAccountParams())
     await expect(promise).rejects.toThrowError(new UnexpectedError())
   })
+
+  test('Should throw UnexpectedError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response.statusCode = HttpStatusCode.serverError
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrowError(new UnexpectedError())
+  })
 })
