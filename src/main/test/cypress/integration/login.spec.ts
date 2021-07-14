1ef4ba3c874148775d0ce9dec24376c3650745ea
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-07-14 14:09:04 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -5,5 +5,8 @@ describe('Login', () => {
 
   it('Should load with correct initial state', () => {
     cy.getByTestId('email-status').should('have.attr', 'title', 'Campo obrigatório')
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Campo obrigatório')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
