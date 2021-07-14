fd31c51451e818fdb47a7c4ab7c27b3a8367b4b4
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-07-14 19:20:42 -0300

    refactor: improve input layout

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -14,31 +14,30 @@ const Input: React.FC<Props> = (props: Props) => {
       [event.target.name]: event.target.value
     })
   }
-  const getStatusClass = (): string => {
-    return error ? Styles.statusError : Styles.statusSuccess
+
+  const getStatus = (): string => {
+    return error ? 'invalid' : 'valid'
   }
-  const getTitle = (): string => {
-    return error || 'Campo válido'
-  }
+
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
   }
   return (
-    <div className={Styles.inputWrap}>
+    <div
+      data-testid={`${props.name}-wrap`}
+      className={Styles.inputWrap}
+      data-status={getStatus()}
+    >
       <input
         {...props}
         placeholder=" "
+        title={error}
         data-testid={props.name}
         readOnly
         autoComplete="off"
         onFocus={enableInput}
         onChange={handleChange} />
-        <label>{props.placeholder}</label>
-      <span
-        data-testid={`${props.name}-status`}
-        title={getTitle()}
-        className={getStatusClass()}
-      ></span>
+        <label data-testid={`${props.name}-label`}>{props.placeholder}</label>
     </div>
   )
 }
