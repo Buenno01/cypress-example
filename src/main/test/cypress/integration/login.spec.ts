154a7d5511dc2205a04aaf3eb439004c46adf973
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-07-14 14:24:19 -0300

    test: ensure login presents error state if invalid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,7 @@
 import faker from 'faker'
 
+const baseUrl: string = Cypress.config().baseUrl
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -23,12 +25,24 @@ describe('Login', () => {
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
-  it('Should present error state if form is valid', () => {
+  it('Should present valid state if form is valid', () => {
     cy.getByTestId('email').focus().type(faker.internet.email())
     cy.getByTestId('email-status').should('have.attr', 'title', 'Campo válido')
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
     cy.getByTestId('password-status').should('have.attr', 'title', 'Campo válido')
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if invalid credentials are provided', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('loader').should('exist')
+      .getByTestId('main-error').should('not.exist')
+      .getByTestId('loader').should('not.exist')
+      .getByTestId('main-error').should('contain.text', 'Credenciais inválidas')
+    cy.url().should('equal', `${baseUrl}/login`)
   })
 })
