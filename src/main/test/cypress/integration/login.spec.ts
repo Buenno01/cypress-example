19c80578c7eca41fafdcf020cdcaa52fc95f1eeb
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-07-14 14:16:51 -0300

    test: ensure login presents error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,11 +1,24 @@
+import faker from 'faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
 
   it('Should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readOnly')
     cy.getByTestId('email-status').should('have.attr', 'title', 'Campo obrigatório')
+    cy.getByTestId('password').should('have.attr', 'readOnly')
     cy.getByTestId('password-status').should('have.attr', 'title', 'Campo obrigatório')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word())
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo inválido')
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Tamanho mínimo de caracteres inválido')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
