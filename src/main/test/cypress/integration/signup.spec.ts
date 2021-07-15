77df837cd0f833c48a2fa015b273e6566bf32fbf
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-07-15 16:24:03 -0300

    test: ensure SignUp present error state if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,4 +1,5 @@
 import * as Helper from '../support/form-helper'
+import faker from 'faker'
 
 describe('SignUp', () => {
   beforeEach(() => {
@@ -14,6 +15,19 @@ describe('SignUp', () => {
     Helper.testInputStatus('password', 'Campo obrigatório')
     cy.getByTestId('passwordConfirmation').should('have.attr', 'readOnly')
     Helper.testInputStatus('passwordConfirmation', 'Campo obrigatório')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('name').focus().type(faker.random.alphaNumeric(3))
+    Helper.testInputStatus('name', 'Tamanho mínimo de caracteres inválido')
+    cy.getByTestId('email').focus().type(faker.random.word())
+    Helper.testInputStatus('email', 'Campo inválido')
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    Helper.testInputStatus('password', 'Tamanho mínimo de caracteres inválido')
+    cy.getByTestId('passwordConfirmation').focus().type(faker.random.alphaNumeric(4))
+    Helper.testInputStatus('passwordConfirmation', 'Campo inválido')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
