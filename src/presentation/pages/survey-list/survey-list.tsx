3b1193138117a499d9b21de54a19fa84c0ce45c3
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-07-18 19:02:05 -0300

    refactor: adapt layout to mobile

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -17,6 +17,30 @@ const SurveyList: React.FC = () => {
                 <span className={Styles.month}>07</span>
                 <span className={Styles.year}>2021</span>
               </time>
+              <p>Imposto é roubo?Imposto é roubo?Imposto é roubo?Imposto é roubo?Imposto é roubo?Imposto é roubo?Imposto é roubo? Imposto é roubo?Imposto é roubo?Imposto é roubo?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbDown} />
+              <time>
+                <span className={Styles.day}>15</span>
+                <span className={Styles.month}>07</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+              <p>Imposto é roubo?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbDown} />
+              <time>
+                <span className={Styles.day}>15</span>
+                <span className={Styles.month}>07</span>
+                <span className={Styles.year}>2021</span>
+              </time>
               <p>Imposto é roubo?</p>
             </div>
             <footer>Ver Resultado</footer>
