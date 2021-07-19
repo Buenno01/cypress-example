20a5e918dd26fcec4ce892b384325d8aa0e449c0
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-07-19 17:24:50 -0300

    feat: ensure SurveyList Component presents 4 empty items on start

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,16 +1,15 @@
 import { Footer, Header } from '@/presentation/components'
 import React from 'react'
-import { SurveyItem, SurveyItemEmpty } from './components'
+import { SurveyItemEmpty } from './components'
 import Styles from './survey-list-styles.scss'
 
 const SurveyList: React.FC = () => {
   return (
-    <div className={Styles.surveyListWrap}>
+    <div data-testid="survey-list" className={Styles.surveyListWrap}>
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul>
-          <SurveyItem />
           <SurveyItemEmpty />
         </ul>
       </div>
